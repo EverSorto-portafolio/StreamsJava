f19f90a7710d2e38eef52755732f3952d274168b
EverSorto-portafolio <everalfredo.sorto@gmail.com>
2025-03-10 23:45:05 -0600

    Guia 2 interfaz y clases anonimas

--- src/main/java/com/mycompany/borrador1/Borrador1.java
+++ src/main/java/com/mycompany/borrador1/Borrador1.java
@@ -6,27 +6,51 @@ package com.mycompany.borrador1;
 import Modelos.EjemploTipo1;
 import Modelos.Modelo2;
 import Services.InterfaceDB1;
+import java.lang.reflect.Field;
 
 public class Borrador1 {
-
+    
     public static void main(String[] args) {
-           implementarClassInterfaz  obj = new implementarClassInterfaz();
-           ImplementacionClase2 obj2 = new ImplementacionClase2();  
-           
-          
-           obj.getAll();  //set  marca
-           obj.getById(1);  // get marca 
-           obj.insert("");  //set  empresa 
-           obj.Update("Interfaz1"); // getempresa    
-           System.out.println(obj.all());
-         
-        System.out.println("----------------------------");
+        implementarClassInterfaz obj = new implementarClassInterfaz();
+        ImplementacionClase2 obj2 = new ImplementacionClase2();        
         
-           obj2.getAll();  //set  marca
-           obj2.getById(1);  // get marca 
-           obj2.insert("");  //set  empresa 
-           obj2.Update("Interfaz1"); // getempresa    
-           System.out.println(obj2.all());
+        System.out.println(implementarClassInterfaz.class.getName());
+        System.out.println(ImplementacionClase2.class.getName());
+        
+        Field[] fields = ImplementacionClase2.class.getDeclaredFields();
+        
+        for (Field field : fields) {
+            System.out.println("nombre " + field.getName());
+            System.out.println("Tipo " + field.getType());
+        }
+        
+        InterfaceDB1<String> temp = new InterfaceDB1<String>() {
+            @Override
+            public String getAll() {
+                throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+            }
             
+            @Override
+            public String getById(int id) {
+                throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+            }
+            
+            @Override
+            public String insert(String objeto) {
+                throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+            }
+            
+            @Override
+            public String Update(String objeto) {
+                throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+            }
+            
+            @Override
+            public String delete(String objeto) {
+                throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+            }
+        };
+        
+        System.out.print("Pertenece a  " + temp.getClass().getName());
     }
 }
