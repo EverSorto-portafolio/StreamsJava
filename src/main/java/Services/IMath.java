7293c2b8fc4bdec276125de57a8cfd0fa12648b1
EverSorto-portafolio <everalfredo.sorto@gmail.com>
2025-03-11 21:07:39 -0600

    Guia 2 se crea la interfaz

--- src/main/java/Services/IMath.java
+++ src/main/java/Services/IMath.java
@@ -4,4 +4,11 @@ package Services;
 @FunctionalInterface
 public interface IMath {
     
+    Double execute (double e , double b);
+    
+    default double suma(double e , double b){
+    
+    return a + b
+    };
+    
 }
