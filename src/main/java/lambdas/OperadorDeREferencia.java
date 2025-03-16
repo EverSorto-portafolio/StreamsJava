57a1e33484f4f14a817c53996f6911eb1123926f
EverSorto-portafolio <everalfredo.sorto@gmail.com>
2025-03-16 15:20:11 -0600

    Guia 3 Referencia al metodo utilizando lambdas para ejecutar el proceso :: con progrmacion funcional desde el constructor

--- src/main/java/lambdas/OperadorDeREferencia.java
+++ src/main/java/lambdas/OperadorDeREferencia.java
@@ -6,6 +6,7 @@ package lambdas;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.UUID;
 import java.util.function.Supplier;
 import java.util.stream.IntStream;
@@ -19,13 +20,26 @@ public class OperadorDeREferencia {
     public static void main(String[] args) {
         List<Integer> numbers = new ArrayList<>(10);
         IntStream repetir = IntStream.range(1, 11);
-     // repetir.forEach(i ->  numbers.add(i));
+        // repetir.forEach(i ->  numbers.add(i));
         repetir.forEach(numbers::add);
         System.out.println(numbers);
-          
-        Supplier<UUID> getToken  = UUID::randomUUID;
+
+        Supplier<UUID> getToken = UUID::randomUUID;
         System.out.println(getToken.get());
         
+        Supplier<Prueba1> referenciaConstructor =  () -> new Prueba1();
+        System.out.println(referenciaConstructor.get());
+    }
+}
+
+class Prueba1 {
+
+    private String id;
+    private Integer edad;
+
+    public Prueba1() {
+        this.id = UUID.randomUUID().toString();
+        this.edad = new Random().nextInt(99);
     }
 
 }
