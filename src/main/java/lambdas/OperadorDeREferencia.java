f1d6cdd164919b2152bfaea7ec71e9bd553744da
EverSorto-portafolio <everalfredo.sorto@gmail.com>
2025-03-15 23:28:04 -0600

    Guia 3 Referencia a métodos se crear el metodo para recorrer el arrayList de la manera tradicional

---
+++ src/main/java/lambdas/OperadorDeREferencia.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lambdas;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+/**
+ *
+ * @author everc
+ */
+public class OperadorDeREferencia {
+
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>(10);
+        IntStream repetir = IntStream.range(1, 11);
+        repetir.forEach(i ->  numbers.add(i));
+        System.out.println(numbers);
+    }
+
+}
