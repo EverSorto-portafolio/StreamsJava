ece8cbc1c4de1594c57884ac35c4871ef07c4f41
EverSorto <everalfredo.sorto@gmail.com>
2025-03-18 23:28:49 -0600

    guia 4 se ejecuta el metodo compose

--- src/main/java/lambdas/funcionesEjemplo.java
+++ src/main/java/lambdas/funcionesEjemplo.java
@@ -8,10 +8,11 @@ public class funcionesEjemplo {
     static  Function<Integer, Integer> multiplicar =  n -> n * 10;
     static  Function<Integer, Integer>  sumar = n -> n + 10;
     static Function<Integer, Integer> usoandThen = multiplicar.andThen(sumar);
-    
+    static  Function <Integer, Integer> usoCompose = multiplicar.compose(sumar);
     public static void main(String[] args) {
         System.out.println(multiplicar.apply(10));
         System.out.println(sumar.apply(10));
-        System.err.println(usoandThen.apply(3));
+        System.out.println(usoandThen.apply(3));
+           System.out.println(usoCompose.apply(3));
     }
 }
