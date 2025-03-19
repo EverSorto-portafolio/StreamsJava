81700eb8951d29879b5753deb7543436635bb69a
EverSorto <everalfredo.sorto@gmail.com>
2025-03-18 22:57:40 -0600

    guia 4 se ejecuta el metodo andthen he inicio con la implementacion de compose

--- src/main/java/lambdas/funcionesEjemplo.java
+++ src/main/java/lambdas/funcionesEjemplo.java
@@ -7,10 +7,11 @@ public class funcionesEjemplo {
     
     static  Function<Integer, Integer> multiplicar =  n -> n * 10;
     static  Function<Integer, Integer>  sumar = n -> n + 10;
+    static Function<Integer, Integer> usoandThen = multiplicar.andThen(sumar);
     
     public static void main(String[] args) {
         System.out.println(multiplicar.apply(10));
         System.out.println(sumar.apply(10));
+        System.err.println(usoandThen.apply(3));
     }
-
 }
