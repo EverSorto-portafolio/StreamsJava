e95e097f68335ad0ff39506ec27c67b71c3c6f57
EverSorto <everalfredo.sorto@gmail.com>
2025-03-24 09:14:45 -0600

    guia 4 se finaliza con una serializacion y deserealizacion  del objeto en el main

--- src/main/java/lambdas/Serializacion2.java
+++ src/main/java/lambdas/Serializacion2.java
@@ -1,9 +1,12 @@
 package lambdas;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
+import java.util.Arrays;
 import java.util.function.Function;
 import lombok.ToString;
 
@@ -23,7 +26,29 @@ public class Serializacion2 {
             System.err.println("lambdas.Serializacion2.methodName()" + ioe.getMessage());
         }
         return inMemoryBytes;
-        };
+    };
+
+    static Function<ByteArrayInputStream, Person> deserializer  = bais -> {
+        try (ObjectInputStream objectInputStream = new ObjectInputStream(bais)) {
+            return (Person) objectInputStream.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            throw  new RuntimeException(e.getMessage());
+        }
+
+    };
+    
+    
+    public static void main(String[] args) {
+        ByteArrayOutputStream  objetoSerealizable =  serializer.apply(new Person("Ever" , 30));
+        
+        System.out.println(Arrays.toString(objetoSerealizable.toByteArray()));
+        
+        Person  objetoDeserializado  = deserializer.apply
+        (new ByteArrayInputStream(objetoSerealizable.toByteArray()));
+        
+        System.out.println(objetoDeserializado.toString());
+    }
+
 }
 
 @ToString
@@ -36,4 +61,8 @@ class Person implements Serializable {
         this.age = age;
         this.nombre = nombre;
     }
+     
+    public String toString(){
+    return  "nombre: " + nombre + " edad: " + age;
+            }
 }
